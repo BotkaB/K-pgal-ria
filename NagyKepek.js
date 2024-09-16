5fbb09e56a34c1bc045057d2b6b56cf8ee2166be
BotkaB <botikabinaka@gmail.com>
2024-09-16 11:13:28 +0200

    nagykép  index keresés beépítése

--- NagyKepek.js
+++ NagyKepek.js
@@ -4,35 +4,34 @@ import { kepLista } from "./adatok.js";
 
 export default class NagyKepek {
     #lista = [];
-   
+    #index = 0;
+
+
 
     constructor(lista, szuloElem) {
         this.#lista = lista;
+        // console.log(lista);
         this.szuloElem = szuloElem;
-       // this.szuloElem.empty();
-      
-        this.#kepCsere();
 
+        // console.log(this.#index);
+        this.#kepCsere();
     }
 
 
     #kepCsere() {
-        
-       new Kep(this.#lista[0], this.szuloElem);
-      
+
+        new Kep(this.#lista[0], this.szuloElem);
+
         $(window).on("kivalaszt", (event) => {
-            console.log(event.detail) 
+            console.log(event.detail)
+            this.#index = this.#lista.findIndex(Kep => Kep.url === event.detail.url && Kep.nev === event.detail.nev)
+
+            console.log(this.#index);
             this.szuloElem.empty();
-           
+
             new Kep(event.detail, this.szuloElem);
         })
-
-
-
     }
-
-
-
 
 }
 
