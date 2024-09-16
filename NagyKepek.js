7aa830d5cf0e36dad5f0eaa45755c56fa8aa018d
BotkaB <botikabinaka@gmail.com>
2024-09-16 12:24:56 +0200

    képnézegető léptetés működik

--- NagyKepek.js
+++ NagyKepek.js
@@ -1,10 +1,12 @@
-import Kepek from "./Kepek.js";
+
 import Kep from "./Kep.js";
-import { kepLista } from "./adatok.js";
+
 
 export default class NagyKepek {
     #lista = [];
     #index = 0;
+    #balGomb;
+    #jobbGomb;
 
 
 
@@ -12,26 +14,52 @@ export default class NagyKepek {
         this.#lista = lista;
         // console.log(lista);
         this.szuloElem = szuloElem;
-
-        // console.log(this.#index);
+        this.#balGomb = $('.bal-gomb');
+        this.#jobbGomb = $('.jobb-gomb');
+        this.#balGomb.on('click', () => this.#balraLep());
+        this.#jobbGomb.on('click', () => this.#jobbraLep());
+        this.#kepFrissit();
         this.#kepCsere();
+
+    }
+
+    #kepFrissit() {
+        this.szuloElem.empty();
+        new Kep(this.#lista[this.#index], this.szuloElem);
     }
 
 
-    #kepCsere() {
 
-        new Kep(this.#lista[0], this.szuloElem);
+    #kepCsere() {
 
         $(window).on("kivalaszt", (event) => {
             console.log(event.detail)
             this.#index = this.#lista.findIndex(Kep => Kep.url === event.detail.url && Kep.nev === event.detail.nev)
 
             console.log(this.#index);
-            this.szuloElem.empty();
+           // this.szuloElem.empty();
 
-            new Kep(event.detail, this.szuloElem);
+            //new Kep(event.detail, this.szuloElem);
+            this.#kepFrissit();
         })
     }
+
+
+
+
+
+    #balraLep() {
+        this.#index = (this.#index > 0) ? this.#index - 1 : this.#lista.length - 1;
+        this.#kepFrissit();
+    }
+
+
+    #jobbraLep() {
+        this.#index = (this.#index < this.#lista.length - 1) ? this.#index + 1 : 0;
+        this.#kepFrissit();
+    }
+
+
 
 }
 
